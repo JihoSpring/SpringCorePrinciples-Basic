699cc23425c4ede73a88bdbcf112b8a30077a82b
Jihogrammer <jiho@kakao.com>
2022-05-08 22:34:19 +0900

    ✨ convert pure java to spring

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
@@ -8,15 +11,18 @@ public class MemberApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        // AppConfig appConfig = new AppConfig();
+        // MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "alpha", Grade.VIP);
         memberService.join(member);
 
         Member foundMember = memberService.findMember(1L);
         System.out.println("New Member is " + member.getName());
-        System.out.println("Found Member is " + foundMember.getName()  );
+        System.out.println("Found Member is " + foundMember.getName());
 
     }
 
