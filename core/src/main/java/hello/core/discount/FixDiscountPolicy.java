be36f7d6681b82b20ebe3c8080b4aac78e53dab0
Jihogrammer <jiho@kakao.com>
2022-05-15 17:46:25 +0900

    ✏️ learn about Primary Annotation

--- core/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -1,13 +1,11 @@
 package hello.core.discount;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
 @Component
-@Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private final int discountFixAmount = 1_000; // 1000원 할인
