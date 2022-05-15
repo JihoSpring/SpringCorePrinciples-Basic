aedbbf6d7452b81221c1f503e1411103dee218a4
Jihogrammer <jiho@kakao.com>
2022-05-15 17:41:50 +0900

    ✏️ learn about Qualifier Annotation

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package hello.core.order;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import hello.core.discount.DiscountPolicy;
@@ -19,7 +20,10 @@ public class OrderServiceImpl implements OrderService {
     private final DiscountPolicy discountPolicy;
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(
+        MemberRepository memberRepository,
+        @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy)
+    {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
