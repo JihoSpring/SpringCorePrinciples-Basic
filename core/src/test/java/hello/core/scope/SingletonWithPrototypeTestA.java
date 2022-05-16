62ff5ab685a6c4c6e958a2760271ddca0de7695c
Jihogrammer <jiho@kakao.com>
2022-05-16 21:52:24 +0900

    ✏️ Provider for Prototype Beans

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTestA.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTestA.java
@@ -2,9 +2,11 @@ package hello.core.scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -52,6 +54,38 @@ class SingletonWithPrototypeTestA {
 
     }
 
+    static class ClientBeanWithProvider {
+
+        private final ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+        public ClientBeanWithProvider(ObjectProvider<PrototypeBean> prototypeBeanProvider) {
+            this.prototypeBeanProvider = prototypeBeanProvider;
+        }
+
+        public int login() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+
+    }
+
+    static class ClientBeanWithJSR330Provider {
+
+        private final Provider<PrototypeBean> prototypeBeanProvider;
+
+        public ClientBeanWithJSR330Provider(Provider<PrototypeBean> prototypeBeanProvider) {
+            this.prototypeBeanProvider = prototypeBeanProvider;
+        }
+
+        public int login() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+
+    }
+
     ConfigurableApplicationContext context;
 
     @Test
@@ -74,6 +108,24 @@ class SingletonWithPrototypeTestA {
         assertThat(client1.login()).isEqualTo(1);
         ClientBean client2 = context.getBean(ClientBean.class);
         assertThat(client2.login()).isEqualTo(2);
+    }
+
+    @Test
+    void singleton_bean_has_a_ObjectProvider() {
+        context = new AnnotationConfigApplicationContext(ClientBeanWithProvider.class, PrototypeBean.class);
+        ClientBeanWithProvider client1 = context.getBean(ClientBeanWithProvider.class);
+        assertThat(client1.login()).isEqualTo(1);
+        ClientBeanWithProvider client2 = context.getBean(ClientBeanWithProvider.class);
+        assertThat(client2.login()).isEqualTo(1);
+    }
+
+    @Test
+    void singleton_bean_has_a_JSR330_Provider() {
+        context = new AnnotationConfigApplicationContext(ClientBeanWithJSR330Provider.class, PrototypeBean.class);
+        ClientBeanWithJSR330Provider client1 = context.getBean(ClientBeanWithJSR330Provider.class);
+        assertThat(client1.login()).isEqualTo(1);
+        ClientBeanWithJSR330Provider client2 = context.getBean(ClientBeanWithJSR330Provider.class);
+        assertThat(client2.login()).isEqualTo(1);
     }
 
 }
