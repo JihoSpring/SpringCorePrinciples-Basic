df6041356ba2216aa13ab6d71d90c86af08ec102
Jihogrammer <jiho@kakao.com>
2022-05-16 22:49:27 +0900

    ✏️ Web Scope "request" & Proxy and Lazy DI

---
+++ core/src/main/java/hello/core/common/MyProxyLogger.java
@@ -0,0 +1,9 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class MyProxyLogger extends MyLogger {}
